9790aa3470afc1fe2cd78f44a4a5d1a2046cca0d
jppoullet <jppoullet@yahoo.com>
2024-01-13 19:32:04 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
